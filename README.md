43ff2c7d2e5693d6d08697197c551f1b25feebcd
p <me@peileppe.com>
2017-12-19 16:46:31 +0100

    ..

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
-Apocalypse
-==========
+Apocalypse World DB repository
+![image](https://raw.githubusercontent.com/peileppe/apocalypse/master/character-sheets/grungebar.png)
 
 Apocalypse World database for v1.0
-- angel.odt
-- AW.db contains 3 tables
+- angel.odt (inherited from previous project)
+
+- AW.db contains 4 tables (new)
 	- AWClasses (11 Classes) basic & special 
 	- AWMoves (78 Moves)
 	- AWCharNames (Angel Name - 28 )
+	- AWKeys (1)
 
-
-Features the ruleset Apocalypse World. For more information visit http://apocalypse-world.com/ 	
+- To do 
+	- adding other classes (currently only Angel is featured)
+	- adding tables for Gears (weapons, etc ...)
+	- adding tables for Campaign (threats, etc ...)
+	- adding tables for Players (Characters, stats  etc ...)
+	
+Features the rule set Apocalypse World. For more information visit http://apocalypse-world.com/ 	
 ![image](https://raw.githubusercontent.com/peileppe/apocalypse/master/character-sheets/grungebar.png)
--- sql-scripts/AW-sql-battlebabe-name.sql
+++ sql-scripts/AW-sql-battlebabe-name.sql
@@ -38,4 +38,28 @@ FOREIGN KEY (classId) REFERENCES AWClasses(id)
 ); 
 
 insert into AWCharNames (name, classid) values ('Snow', 2); 
+insert into AWCharNames (name, classid) values ('Crimson',  2); 
+insert into AWCharNames (name, classid) values ('Shadow',  2); 
+insert into AWCharNames (name, classid) values ('Azure',  2); 
+insert into AWCharNames (name, classid) values ('Midnight',  2); 
+insert into AWCharNames (name, classid) values ('Scarlet',  2); 
+insert into AWCharNames (name, classid) values ('Violetta', 2); 
+insert into AWCharNames (name, classid) values ('Amber', 2); 
+insert into AWCharNames (name, classid) values ('Rouge', 2); 
+insert into AWCharNames (name, classid) values ('Damson', 2); 
+insert into AWCharNames (name, classid) values ('Sunset', 2); 
+insert into AWCharNames (name, classid) values ('Emerald', 2); 
+insert into AWCharNames (name, classid) values ('Ruby',2); 
+insert into AWCharNames (name, classid) values ('Raksha',  2); 
+insert into AWCharNames (name, classid) values ('Kickskirt',  2); 
+insert into AWCharNames (name, classid) values ('Kite',  2); 
+insert into AWCharNames (name, classid) values ('Monsoon',  2); 
+insert into AWCharNames (name, classid) values ('Smith',  2); 
+insert into AWCharNames (name, classid) values ('Beastie',  2); 
+insert into AWCharNames (name, classid) values ('Baaba', 2); 
+insert into AWCharNames (name, classid) values ('Melody', 2); 
+insert into AWCharNames (name, classid) values ('Mara', 2); 
+insert into AWCharNames (name, classid) values ('Tavin', 2); 
+insert into AWCharNames (name, classid) values ('Absinthe', 2); 
+insert into AWCharNames (name, classid) values ('Honeytree', 2); 
 
