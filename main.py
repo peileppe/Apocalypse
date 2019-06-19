0d8465383870b51cc77b72204353e3fde59c6029
peileppe <peileppeproduction@gmail.com>
2019-06-19 16:48:41 +0200

    --

--- main.py
+++ main.py
@@ -21,31 +21,35 @@ class unit(object):
     def found(self, s):
         return (self.active and (s.lower() in self.name.lower()))
     def display(self):
-        if self.active: return ('<%s  \t%s   >' % (self.name,self.classe))
+        if self.active: return self.name+" "+self.classe
 
 class character(unit):
-    def __init__(self, n, c):
+    def __init__(self, n, c, hard=0, sharp=0, hot=0, cool=0, weird=0, sk=''):
         unit.__init__(self, n,c)
         self.level = 1
         self.hp = 10
         self.countdown = {}
-        self.hard = 0
-        self.sharp= 0
-        self.hot  = 0
-        self.cool = 0
-        self.weird= 0
+        self.stats = [hard, sharp, hot, cool, weird] # str(hard)0, int(sharp)1, chm(hot)2, dex(cool)3, psi(weird)4
+        self.skill = sk
         return
     def display(self):
-        return unit.display(self)+'['+str(self.level)+']['+str(self.hp)+']'+'hard:'+str(self.hard)+'sharp:'+str(self.sharp)+'hot:'+str(self.hot)+'cool:'+str(self.cool)
+        r = unit.display(self)
+        r+= '['+str(self.level)+']['+str(self.hp)+']'
+        r+= 'stats:'+','.join(str(self.stats[i]) for i in range(len(self.stats)))
+        r+= 'skill:'+self.skill
+        return r
 
 class player():
     def __init__(self):
         self.characters = []
         self.countdowns = {}
         return
-    def starterPack(self, chList):
+    def starterPack(self, awcls):
+        chList=[]
+        for c in awcls:
+            chList.append(c)
         for i, k in enumerate(chList):
-            self.characters.append(character('name='+str(i), k[1]))
+            self.characters.append(character('name='+str(i), k, awcls[k]['stat'][0],awcls[k]['stat'][1],0,0,0))
         return
 
 def listing(ul):
@@ -60,9 +64,8 @@ def main():
     except IOError:
         print(IOError)
     p = player()
-    p.starterPack(r1)
+    p.starterPack(awcl)
     listing(p.characters)
-    sqlDb.close() 
     return
 
 if __name__ == "__main__":
