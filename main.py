87c6af83d1631dcee40fd4dd804c877681e58757
peileppe <peileppeproduction@gmail.com>
2019-06-20 11:10:23 +0200

    -- main using import

--- main.py
+++ main.py
@@ -6,38 +6,8 @@
 import readline
 import json
 from aw_log import logger
-
-try:
-    input = raw_input
-except NameError:
-    pass
-
-class unit(object):
-    def __init__(self, name='', classe=''):
-        self.name=name
-        self.classe=classe
-        self.active=True
-        return
-    def found(self, s):
-        return (self.active and (s.lower() in self.name.lower()))
-    def display(self):
-        if self.active: return self.name.ljust(10)+" "+self.classe.ljust(10)
-
-class character(unit):
-    def __init__(self, n, c, st=[0, 0, 0, 0, 0], sk=''):
-        unit.__init__(self, n,c)
-        self.level = 1
-        self.hp = 10
-        self.countdown = {}
-        self.stats = st # str(hard)0, int(sharp)1, chm(hot)2, dex(cool)3, psi(weird)4
-        self.skill = sk
-        return
-    def display(self):
-        r = unit.display(self)+'\t'
-        r+= '['+str(self.level)+']['+str(self.hp)+']'+'\t'
-        r+= 'stats:'+','.join(str(self.stats[i]) for i in range(len(self.stats))).ljust(10)+'\t'
-        r+= 'skill:'+self.skill
-        return r
+from aw_tools import menu
+from aw_character import character, listing
 
 class player():
     def __init__(self):
@@ -51,11 +21,6 @@ class player():
         for i, k in enumerate(chList):
             self.characters.append(character('name='+str(i), k, awcls[k]['stat'],awcls[k]['skill']))
         return
-
-def listing(ul):
-    for i in ul:
-        print(i.display())
-    return
 
 def main():
     try:
